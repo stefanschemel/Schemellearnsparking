5b92e6b1c650c943619b85d9b81e955bf65b89b6
Philipp Schemel <PhilippSchemel@live.com>
2015-04-11 15:54:09 +0200

    Test commit

--- src/gui.java
+++ src/gui.java
@@ -12,84 +12,84 @@ public class gui extends JPanel implements ActionListener
 	Image map;
 	int key,Auto_X,Auto_Y;
 	int x_Car,y_Car;
-//	akjsdkjasbdjkbas
+//	sallleeeeee
 	public gui()
 	{
 		setFocusable(true);
 		ImageIcon u = new ImageIcon("C:/Users/Stefan/Documents/Schule/TG 12-2/CT-Becker/Eclipse/Schemellearnsparking/src/Level1.png");
 		map = u.getImage();
-		
+
 		addKeyListener(new AL());	//AktionListener aufrufen und aktivieren
-		
+
 		Auto_X=60;
 	    Auto_Y=80;
-	    
+
 	    time= new Timer(5,this);
 	    time.start();
 	}
 
 	public void actionPerformed(ActionEvent e)
 	{
-		
+
 	}
-	
+
 	public void paint (Graphics g)
 	{
 		super.paint(g);
 		Graphics2D f2 =(Graphics2D) g;
 		f2.drawImage(map,0,0,null);
-	
+
 		g.setColor(new Color(255,4,4));
 		g.fillRect(Auto_X,Auto_Y,60,20);
 	}
-	
+
 	private class AL extends KeyAdapter
 	{
 			public AL()
 			{
-				
+
 			}
-			
-			public void keyPressed(KeyEvent e)  //tasten drücken
+
+			public void keyPressed(KeyEvent e)  //tasten drï¿½cken
 			{
 				key= e.getKeyCode();
-				
+
 				if(key == KeyEvent.VK_LEFT)
 				{	Auto_X=Auto_X-15;
 					repaint();
 					{
-						
+
 					}
 				}
-				
+
 				if(key == KeyEvent.VK_RIGHT)
 				{	Auto_X=Auto_X+15;
 					repaint();
 				}
-				
+
 				if(key == KeyEvent.VK_UP)
 				{	Auto_Y=Auto_Y-15;
 					repaint();
 				}
-				
+
 				if(key == KeyEvent.VK_DOWN)
 				{	Auto_Y=Auto_Y+15;
 					repaint();
 				}
 			}
-		
-			public void keyReleased(KeyEvent e)  //Tasten losgelassen			
+
+			public void keyReleased(KeyEvent e)  //Tasten losgelassen
 			{
 				key=e.getKeyCode();
-				
+
 				if(key==KeyEvent.VK_LEFT)
 				{
-					
+
 				}
-				
+
 				if(key==KeyEvent.VK_RIGHT)
 				{
-					
+
 				}
 			}
 	}
@@ -98,7 +98,7 @@ public class gui extends JPanel implements ActionListener
 	{
 		return x_Car;
 	}
-	
+
 	public int getYCar()
 	{
 		return y_Car;
